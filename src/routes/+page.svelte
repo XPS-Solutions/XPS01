0973d6bf568f828ac03e093d9c53de4600330ff1
PablosGon <100768678+PablosGon@users.noreply.github.com>
2025-02-28 18:25:27 +0100

    main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import type { TaskModel } from "../models/domain/task.model";
+	import { State } from "../enums/state.enum";
+	import type { TaskViewModel } from "../models/view/task.view-model";
+	
+    let showNewTaskForm: boolean = $state(false);
+    let tasks: TaskModel[] = $state([])
+
+    let newTask: TaskViewModel = $state({
+        title: '',
+        description: '',
+    });
+    let addTask = () => {
+        showNewTaskForm = false;
+        tasks.unshift({
+            title: newTask.title,
+            description: newTask.description,
+            state: State.Pending,
+            created: new Date()
+        });
+    }
+</script>
+
+<main>
+    <button onclick={() => showNewTaskForm = !showNewTaskForm }>Add Task</button>
+    {#if showNewTaskForm}
+        <form onsubmit={addTask}>
+            <label>
+                Title: 
+                <input type="text" bind:value={newTask.title}/>
+            </label>
+            <label>
+                Description:
+                <input type="text" bind:value={newTask.description}/>
+            </label>
+            <button>Add</button>
+        </form>
+    {/if}
+    <ul>
+        {#each tasks as task}
+            <li>
+                <h2>{task.title}</h2>
+                <p>{task.description}</p>
+            </li>
+        {/each}
+    </ul>
+</main>
+
+<style>
+    
+</style>

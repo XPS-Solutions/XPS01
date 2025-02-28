f7d64870a4902ee4a1bcdeb00c5177ce1e05565e
PablosGon <100768678+PablosGon@users.noreply.github.com>
2025-02-28 20:17:12 +0100

    tasks completion (#3)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,14 @@
 	
     let showNewTaskForm: boolean = $state(false);
     let tasks: TaskModel[] = $state([])
+    let pendingTasks: TaskModel[] = $derived(tasks.filter(x => x.state == State.Pending));
+    let completedTasks: TaskModel[] = $derived(tasks.filter(x => x.state == State.Completed));
 
     let newTask: TaskViewModel = $state({
         title: '',
         description: '',
     });
+
     let addTask = () => {
         showNewTaskForm = false;
         tasks.unshift({
@@ -18,6 +21,14 @@
             state: State.Pending,
             created: new Date()
         });
+    }
+
+    let deleteTask = (id: number) => {
+        tasks.splice(id);
+    }
+
+    let completeTask = (id:number) => {
+        tasks[id].state = State.Completed;
     }
 </script>
 
@@ -36,14 +47,31 @@
             <button>Add</button>
         </form>
     {/if}
-    <ul>
-        {#each tasks as task}
-            <li>
-                <h2>{task.title}</h2>
-                <p>{task.description}</p>
-            </li>
-        {/each}
-    </ul>
+    <details open>
+        <summary>Pending Tasks</summary>
+        <ul>
+            {#each pendingTasks as task, index}
+                <li>
+                    <h2>{task.title}</h2>
+                    <p>{task.description}</p>
+                    <button onclick={() => deleteTask(index)}>Delete</button>
+                    <button onclick={() => completeTask(index)}>Complete</button>
+                </li>
+            {/each}
+        </ul>        
+    </details>
+    <details>
+        <summary>Completed Tasks</summary>
+        <ul>
+            {#each completedTasks as task, index}
+                <li>
+                    <h2>{task.title}</h2>
+                    <p>{task.description}</p>
+                    <button onclick={() => deleteTask(index)}>Delete</button>
+                </li>
+            {/each}
+        </ul>        
+    </details>
 </main>
 
 <style>
